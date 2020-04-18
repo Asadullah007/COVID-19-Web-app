3a2aa9e039bdf023c50a0324e727b9deee94bf51
Asadullah007 <mrasadullah0@gmail.com>
2020-04-19 00:01:06 +0500

    COVID-19 Web app

---
+++ js/scripts.js
@@ -0,0 +1,266 @@
+/*-----------------------------------------------------------------------------------
+
+    Theme Name: Weix
+    Theme URI: http://
+    Description: The Multi-Purpose Onepage Template
+    Author: UI-ThemeZ
+    Author URI: http://themeforest.net/user/UI-ThemeZ
+    Version: 1.0
+
+-----------------------------------------------------------------------------------*/
+
+
+$(function () {
+
+    "use strict";
+
+    var wind = $(window);
+
+
+
+    /* ===============================  scrollIt  =============================== */
+
+    $.scrollIt({
+        upKey: 38,
+        downKey: 40,
+        easing: 'swing',
+        scrollTime: 600,
+        activeClass: 'active',
+        onPageChange: null,
+        topOffset: -70
+    });
+
+
+    /* ===============================  navbar scrolling background  =============================== */
+
+    wind.on("scroll", function () {
+
+        var bodyScroll = wind.scrollTop(),
+            navbar = $(".navbar"),
+            logo = $(".navbar:not(.nav-box) .logo> img");
+
+        if (bodyScroll > 100) {
+
+            navbar.addClass("nav-scroll");
+            logo.attr('src', 'img/logo-dark.png');
+
+        } else {
+
+            navbar.removeClass("nav-scroll");
+            logo.attr('src', 'img/logo-light.png');
+        }
+    });
+
+    /* ===============================  close navbar-collapse when a clicked  =============================== */
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+
+    /* ===============================  progress bar  =============================== */
+
+    wind.on('scroll', function () {
+        $(".skill-progress .progres").each(function () {
+            var bottom_of_object =
+                $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window =
+                $(window).scrollTop() + $(window).height();
+            var myVal = $(this).attr('data-value');
+            if (bottom_of_window > bottom_of_object) {
+                $(this).css({
+                    width: myVal
+                });
+            }
+        });
+    });
+
+    /* ===============================  progress bar circle  =============================== */
+
+    var c4 = $('.circle');
+    var myVal = $(this).attr('data-value');
+
+    $(".sk-progress .circle").each(function () {
+
+        c4.circleProgress({
+            startAngle: -Math.PI / 4 * 2,
+            value: myVal,
+            fill: {
+                gradient: ["#EE3158", "#e1284e"]
+            }
+        });
+
+    });
+
+
+    /* ===============================  sections background image from data background  =============================== */
+
+    var pageSection = $(".bg-img, section");
+    pageSection.each(function (indx) {
+
+        if ($(this).attr("data-background")) {
+            $(this).css("background-image", "url(" + $(this).data("background") + ")");
+        }
+    });
+
+
+    /* ===============================  owlCarousel  =============================== */
+
+    // Testimonials owlCarousel
+    $('.testimonials .owl-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        margin: 15,
+        mouseDrag: false,
+        autoplay: true,
+        smartSpeed: 500
+    });
+
+    // Featurse img
+    $('.fimg .owl-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        margin: 0,
+        animateOut: 'fadeOut',
+        mouseDrag: false,
+        autoplay: true,
+        dots: false,
+        smartSpeed: 500
+    });
+
+
+    /* ===============================  magnificPopup  =============================== */
+
+    $('.gallery').magnificPopup({
+        delegate: '.popimg',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    /* ===============================  countUp  =============================== */
+
+    $('.numbers .count').countUp({
+        delay: 10,
+        time: 1500
+    });
+
+    /* ===============================  YouTubePopUp  =============================== */
+
+    $("a.vid").YouTubePopUp();
+
+
+    /* ===============================  parallaxie  =============================== */
+
+    $('.parallaxie').parallaxie({
+        speed: 0.6,
+        size: "cover"
+    });
+
+
+});
+
+
+/* ===============================  Window after loading  =============================== */
+
+$(window).on("load", function () {
+
+    var wind = $(window);
+
+    // Preloader
+    $(".loading").fadeOut(500);
+
+
+    // isotope
+    $('.gallery').isotope({
+        itemSelector: '.items'
+    });
+
+    var $gallery = $('.gallery').isotope();
+
+    // filter items on button click
+    $('.filtering').on('click', 'span', function () {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $gallery.isotope({ filter: filterValue });
+
+    });
+
+    $('.filtering').on('click', 'span', function () {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
+
+    // contact form validator
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data) {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    });
+
+});
+
+
+/* ===============================  slider  =============================== */
+
+$(document).ready(function() {
+
+    var owl = $('.header .owl-carousel');
+
+
+    // Slider owlCarousel
+    $('.slider .owl-carousel').owlCarousel({
+        items: 1,
+        loop:true,
+        margin: 0,
+        autoplay:true,
+        smartSpeed:1000
+    });
+
+    // Slider owlCarousel
+    $('.slider-fade .owl-carousel').owlCarousel({
+        items: 1,
+        loop:true,
+        margin: 0,
+        autoplay:true,
+        smartSpeed:500,
+        animateOut: 'fadeOut'
+    });
+
+    owl.on('changed.owl.carousel', function(event) {
+        var item = event.item.index - 2;     // Position of the current item
+        $('h5').removeClass('animated fadeInLeft');
+        $('h1').removeClass('animated fadeInRight');
+        $('p').removeClass('animated fadeInUp');
+        $('.butn').removeClass('animated zoomIn');
+        $('.owl-item').not('.cloned').eq(item).find('h5').addClass('animated fadeInLeft');
+        $('.owl-item').not('.cloned').eq(item).find('h1').addClass('animated fadeInRight');
+        $('.owl-item').not('.cloned').eq(item).find('p').addClass('animated fadeInUp');
+        $('.owl-item').not('.cloned').eq(item).find('.butn').addClass('animated zoomIn');
+    });
+
+});
+
